91562c9c86009614ba2cb0666425824a038eff52
David Oladipo <David.Oladipo@outlook.com>
2022-03-22 12:28:46 -0600

    Setting up project dependencies and template

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers(){
+            var users = _context.Users.ToList();
+            return users;
+        }
+
+        
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<AppUser>> GetUsers(int id){
+            var users = _context.Users.ToList();
+            return users;
+        }
+    }
+}
